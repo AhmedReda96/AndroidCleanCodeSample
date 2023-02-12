aebf6549af46ad6232dd706c4fd1a8d409749de1
AhmedRedaUC <95164952+AhmedRedaUC@users.noreply.github.com>
2023-02-12 15:25:48 +0200

    get result

--- app/src/main/java/com/example/uctask/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/uctask/ui/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.uctask.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -11,12 +12,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.example.domain.model.baseViewState.NetworkingViewState
 import com.example.domain.model.pokemonItemModel.PokemonRequest
 import com.example.uctask.theme.UCTaskTheme
 import com.example.uctask.viewModel.PokemonListVM
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -35,18 +38,23 @@ class MainActivity : ComponentActivity() {
         }
 
         viewModel.getPokemon(PokemonRequest(), NetworkingViewState.Loading())
-viewModel.pokemonListLD.collect()
-}
+        lifecycleScope.launch {
+            viewModel.pokemonListLD.collect {
+                Log.d("TAG", "onCreate: ${it.request}")
+            }
+        }
+    }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!")
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    UCTaskTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        UCTaskTheme {
+            Greeting("Android")
+        }
     }
 }
